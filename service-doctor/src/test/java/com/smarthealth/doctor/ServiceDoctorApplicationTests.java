b3c1da45c84492140c240d5e20221558e9d2dace
Janith <jjayawardene610@gmail.com>
2026-03-22 12:07:08 +0530

    feat: initialize service-doctor and service-patient modules with Spring Boot
    - Added pom.xml for service-doctor with dependencies for Spring Boot, JPA, and PostgreSQL.
    - Created main application class ServiceDoctorApplication for service-doctor.
    - Added application.properties to set application name for service-doctor.
    - Implemented basic test class ServiceDoctorApplicationTests for service-doctor.
    
    - Added pom.xml for service-patient with dependencies for Spring Boot, JPA, and PostgreSQL.
    - Created main application class ServicePatientApplication for service-patient.
    - Added application.properties to set application name for service-patient.
    - Implemented basic test class ServicePatientApplicationTests for service-patient.
    
    - Included Maven wrapper files and configurations for both modules.
    - Added .gitignore and .gitattributes for service-patient to manage ignored files and line endings.

---
+++ service-doctor/src/test/java/com/smarthealth/doctor/ServiceDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthealth.doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
