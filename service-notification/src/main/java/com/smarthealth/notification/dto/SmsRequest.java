4331c1398ca6395e7b55172c8c590cdd5cf983ce
SILVA-NNS <nimnasilva29@gmail.com>
2026-04-01 23:26:30 +0530

    fix: update phone validation for notify.lk

--- service-notification/src/main/java/com/smarthealth/notification/dto/SmsRequest.java
+++ service-notification/src/main/java/com/smarthealth/notification/dto/SmsRequest.java
@@ -1,15 +1,13 @@
 package com.smarthealth.notification.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class SmsRequest {
 
     @NotBlank(message = "Phone number is required")
-    @Pattern(regexp = "^\\+[1-9]\\d{1,14}$", message = "Phone must be in E.164 format e.g. +94771234567")
-    private String to;
+    private String to; // e.g. 0771234567 or 94771234567
 
     @NotBlank(message = "Message is required")
     private String message;
