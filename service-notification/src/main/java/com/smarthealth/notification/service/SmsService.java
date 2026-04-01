d3def980dfd2df79a3e8eec775e12e9282ced7d0
SILVA-NNS <nimnasilva29@gmail.com>
2026-04-01 23:22:52 +0530

    feat: implement SMS via notify.lk API

--- service-notification/src/main/java/com/smarthealth/notification/service/SmsService.java
+++ service-notification/src/main/java/com/smarthealth/notification/service/SmsService.java
@@ -1,25 +1,27 @@
 package com.smarthealth.notification.service;
 
-import com.smarthealth.notification.config.TwilioConfig;
+import com.smarthealth.notification.config.NotifyLkConfig;
 import com.smarthealth.notification.dto.SmsRequest;
 import com.smarthealth.notification.entity.NotificationLog;
 import com.smarthealth.notification.enums.NotificationStatus;
 import com.smarthealth.notification.enums.NotificationType;
 import com.smarthealth.notification.repository.NotificationLogRepository;
-import com.twilio.exception.ApiException;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SmsService {
 
-    private final TwilioConfig twilioConfig;
+    private final NotifyLkConfig notifyLkConfig;
     private final NotificationLogRepository logRepository;
+    private final RestTemplate restTemplate;
+
+    private static final String NOTIFY_LK_URL = "https://app.notify.lk/api/v1/send";
 
     public NotificationLog sendSms(SmsRequest request) {
         NotificationLog logEntry = NotificationLog.builder()
@@ -31,16 +33,21 @@ public class SmsService {
                 .build();
 
         try {
-            Message message = Message.creator(
-                    new PhoneNumber(request.getTo()),
-                    new PhoneNumber(twilioConfig.getPhoneNumber()),
-                    request.getMessage()
-            ).create();
+            String url = UriComponentsBuilder.fromHttpUrl(NOTIFY_LK_URL)
+                    .queryParam("user_id", notifyLkConfig.getUserId())
+                    .queryParam("api_key", notifyLkConfig.getApiKey())
+                    .queryParam("sender_id", notifyLkConfig.getSenderId())
+                    .queryParam("to", request.getTo())
+                    .queryParam("message", request.getMessage())
+                    .toUriString();
+
+            String response = restTemplate.getForObject(url, String.class);
+            log.info("Notify.lk response: {}", response);
 
             logEntry.setStatus(NotificationStatus.SENT);
-            log.info("SMS sent to {}. SID: {}", request.getTo(), message.getSid());
+            log.info("SMS sent successfully to {}", request.getTo());
 
-        } catch (ApiException e) {
+        } catch (Exception e) {
             logEntry.setStatus(NotificationStatus.FAILED);
             logEntry.setErrorMessage(e.getMessage());
             log.error("Failed to send SMS to {}: {}", request.getTo(), e.getMessage());
