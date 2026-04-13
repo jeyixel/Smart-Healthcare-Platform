d5785f05d6f88a60e13bcbe0a37260fecfe35273
Heshan Jayasekara <heshanjayasekara18@gmail.com>
2026-04-13 22:31:41 +0530

    feat(kafka): integrate Kafka for patient event logging and consumption

---
+++ service-patient/src/main/java/com/smarthealth/patient/config/KafkaConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.smarthealth.patient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+}
